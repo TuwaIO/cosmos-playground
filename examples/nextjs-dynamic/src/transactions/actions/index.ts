5683fb68710ad869067f634c8010afc0f95ed15b
Alex <argeares97@gmail.com>
2025-09-18 01:25:43 +0300

    fix: update examples with new onSuccessCallback logic

--- examples/nextjs-dynamic/src/transactions/actions/index.ts
+++ examples/nextjs-dynamic/src/transactions/actions/index.ts
@@ -1,6 +1,21 @@
+import { Transaction } from '@tuwaio/pulsar-core';
+
 import { config } from '@/configs/wagmiConfig';
 
 import { increment } from './increment';
+
+export enum TxType {
+  increment = 'increment',
+}
+
+type IncrementTx = Transaction & {
+  type: TxType.increment;
+  payload: {
+    value: number;
+  };
+};
+
+export type TransactionUnion = IncrementTx;
 
 export const txActions = {
   increment: () => increment({ wagmiConfig: config }),
