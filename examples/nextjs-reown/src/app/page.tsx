a157bd9c11ae98a529822f4f318fd405c8789937
Alex <argeares97@gmail.com>
2025-10-21 18:27:08 +0300

    fix: initialize test app and remove nextjs web3auth and nextjs-reown

--- examples/nextjs-reown/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TransactionsBlock } from '@/components/TransactionsBlock';
-
-export default function Home() {
-  return (
-    <div className="w-full flex justify-center items-center min-h-[100dvh]">
-      <TransactionsBlock />
-    </div>
-  );
-}
